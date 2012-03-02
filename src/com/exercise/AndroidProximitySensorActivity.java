7fb6ec6c1484f7a157105bb2e9df4418715a6ad1
Josenivaldo Benito Jr <j.junior@samsung.com>
2012-03-02 17:58:56 -0300

    some cleanup and identation

--- src/com/exercise/AndroidProximitySensorActivity.java
+++ src/com/exercise/AndroidProximitySensorActivity.java
@@ -17,46 +17,49 @@ public class AndroidProximitySensorActivity extends Activity {
 	 SensorManager mySensorManager;
 	 Sensor myProximitySensor;
 	 
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main);
-	        ProximitySensor = (TextView)findViewById(R.id.proximitySensor);
-	        ProximityMax = (TextView)findViewById(R.id.proximityMax);
-	        ProximityReading = (TextView)findViewById(R.id.proximityReading);
-	        
-	        mySensorManager = (SensorManager)getSystemService(
-	          Context.SENSOR_SERVICE);
-	        myProximitySensor = mySensorManager.getDefaultSensor(
-	          Sensor.TYPE_PROXIMITY);
-	        
-	        if (myProximitySensor == null){
-	         ProximitySensor.setText("No Proximity Sensor!"); 
-	        }else{
+
+     public void onCreate(Bundle savedInstanceState) {
+    	 super.onCreate(savedInstanceState);
+	     setContentView(R.layout.main);
+
+	     // TextViews to hold sensor information on the screen
+	     ProximitySensor = (TextView)findViewById(R.id.proximitySensor);
+	     ProximityMax = (TextView)findViewById(R.id.proximityMax);
+	     ProximityReading = (TextView)findViewById(R.id.proximityReading);
+	     
+	     // Globla service SensorManager and Proximity sensor
+	     mySensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+	     myProximitySensor = mySensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+	     
+	     // Test where phone has or has not a proximity sensor
+	     if (myProximitySensor == null){
+	    	 ProximitySensor.setText("No Proximity Sensor!"); 
+	     } else {
 	         ProximitySensor.setText(myProximitySensor.getName());
 	         ProximityMax.setText("Maximum Range: "
 	           + String.valueOf(myProximitySensor.getMaximumRange()));
 	         mySensorManager.registerListener(proximitySensorEventListener,
 	           myProximitySensor,
 	           SensorManager.SENSOR_DELAY_NORMAL);
-	        }
-	    }
-	    
-	    SensorEventListener proximitySensorEventListener
-	    = new SensorEventListener(){
+	     }
+     }
 
-	  public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	   // TODO Auto-generated method stub
-	   
-	  }
+     // Listen for broadcasts from proximity sensor (i.e. every time it changes reading status)
+     SensorEventListener proximitySensorEventListener = new SensorEventListener() {
 
-	  public void onSensorChanged(SensorEvent event) {
-	   // TODO Auto-generated method stub
+		// Called whenever the registered sensor changes its state
+		public void onSensorChanged(SensorEvent event) {
 
-	   if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
-	    ProximityReading.setText("Proximity Sensor Reading:"
-	      + String.valueOf(event.values[0]));
-	   }
-	  }
-	    };
+			 if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
+				 ProximityReading.setText("Proximity Sensor Reading:"
+				 + String.valueOf(event.values[0]));
+			 }
+		}
+
+		// Accuracy is not really necessary, we just looking for status changing
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+	 };
 }
